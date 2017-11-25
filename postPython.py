8547f2346c32b2d63f96b6724d902c56d51d53b5
永田大祐 <nagatadaisuke@nagatadaisuke-no-MacBook-Pro.local>
2017-11-25 22:00:09 +0900

    レスポンスチェック

--- postPython.py
+++ postPython.py
@@ -4,7 +4,9 @@ def main():
     r = requests.get(
         'URL_Setting')
     print (r.status_code)
-    
+    f = open('text.txt', 'w')
+    f.write(str(r.status_code))
+    f.close()
 
 if __name__=='__main__':
     main()
