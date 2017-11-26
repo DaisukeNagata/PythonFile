22c630a4cd45f75f576d1b28a1920eb670b77394
永田大祐 <nagatadaisuke@nagatadaisuke-no-MacBook-Pro.local>
2017-11-27 01:16:01 +0900

    cronによるutf-8対応＋requestsの計測時間

--- postPython.py
+++ postPython.py
@@ -1,15 +1,25 @@
 import requests
+import time
 from datetime import datetime
+# -*- coding: utf-8 -*-
+import sys
+import io
+
 def main():
-    r = requests.get(
-        'URLSetting')
-    print (r.status_code)
+    #cron対応
+    sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
+  
+    start = time.time()
    
-    text = datetime.now().strftime("%Y/%m/%d %H:%M:%S")+"StatusCode"+str(r.status_code)
+    r = requests.get('URLSettings')
+    t = time.time()
+
     
-    f = open('text.txt', 'a')
-    f.write(text + '\n')
-    f.close()
+    elapsed_time =  time.time() - start
+    text = datetime.now().strftime("%Y/%m/%d %H:%M:%S")+" "+"StatusCode"+str(r.status_code)+" "+"経過時間"+str(elapsed_time)
+
+    print(text)
+
 
 if __name__=='__main__':
     main()
