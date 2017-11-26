50f8092ed7ea68177e897664e469a12918ada9c7
永田大祐 <nagatadaisuke@nagatadaisuke-no-MacBook-Pro.local>
2017-11-26 09:56:01 +0900

    ステータスコード取得

--- postPython.py
+++ postPython.py
@@ -1,11 +1,14 @@
 import requests
-
+from datetime import datetime
 def main():
     r = requests.get(
-        'URL_Setting')
+        'URLSetting')
     print (r.status_code)
-    f = open('text.txt', 'w')
-    f.write(str(r.status_code))
+   
+    text = datetime.now().strftime("%Y/%m/%d %H:%M:%S")+"StatusCode"+str(r.status_code)
+    
+    f = open('text.txt', 'a')
+    f.write(text + '\n')
     f.close()
 
 if __name__=='__main__':
