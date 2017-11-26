67d2ad6e54255ffc8610b18ef9f8959623384502
DaisukeNagata <nagatadaisuke@nagatadaisuke-no-MacBook-Pro.local>
2017-11-27 01:23:10 +0900

    cronによるutf-8対応＋requestsの計測時間の表示

--- postPython.py
+++ postPython.py
@@ -12,9 +12,7 @@ def main():
     start = time.time()
    
     r = requests.get('URLSettings')
-    t = time.time()
-
-    
+ 
     elapsed_time =  time.time() - start
     text = datetime.now().strftime("%Y/%m/%d %H:%M:%S")+" "+"StatusCode"+str(r.status_code)+" "+"経過時間"+str(elapsed_time)
 
