ed36414b11ff34365a90f07c2771a297db2d0c73
DaisukeNagata <nagatadaisuke@nagatadaisuke-no-MacBook-Pro.local>
2017-12-05 12:21:28 +0900

    add

--- postPython.py
+++ postPython.py
@@ -11,7 +11,7 @@ def main():
   
     start = time.time()
    
-    r = requests.get('URLSettings')
+    r = requests.get('URLSet')
  
     elapsed_time =  time.time() - start
     text = datetime.now().strftime("%Y/%m/%d %H:%M:%S")+" "+"StatusCode"+str(r.status_code)+" "+"経過時間"+str(elapsed_time)
