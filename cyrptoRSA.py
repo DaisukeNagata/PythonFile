09d1308a4554edb0670c9253f32b6b067d424ce2
daisukenagata <dbank0208@gmail.com>
2017-12-17 00:19:01 +0900

    private-key&public-key

--- cyrptoRSA.py
+++ cyrptoRSA.py
@@ -1,1 +1,23 @@
 from Crypto.PublicKey import RSA
+import Crypto.PublicKey.RSA as RSA
+
+def main():
+    rsa_fact = RSA.RSAImplementation()
+    rsa_key = rsa_fact.generate(bits=2048)
+    
+    key = RSA.construct(map(int, (rsa_key.n,rsa_key.e,rsa_key.d)))
+
+    private = key.exportKey()
+    public = key.publickey().exportKey()
+
+    f = open('private-key.pem', 'wb')
+    f.write(private)
+    f.close()
+
+    f = open('public-key.pem', 'wb')
+    f.write(public)
+    f.close()
+
+if __name__=='__main__':
+    main()
+
