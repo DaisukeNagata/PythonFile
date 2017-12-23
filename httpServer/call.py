5b965f14bae01a565564b8af0872757669bd192b
daisukenagata <dbank0208@gmail.com>
2017-12-23 14:18:58 +0900

    set public or private

--- httpServer/call.py
+++ httpServer/call.py
@@ -12,11 +12,11 @@ def start(callback):
         call(callback, *args)
     server = HTTPServer(('', int(8000)), handler)
     server.serve_forever()
-    #:TDODO
-    print(cyrptoRSA.start_Key())
+
 class call(BaseHTTPRequestHandler):
-    print(cyrptoRSA.start_Key())
+
     def __init__(self, callback, *args):
+
         self.callback = callback
         BaseHTTPRequestHandler.__init__(self, *args)
    
@@ -26,7 +26,10 @@ class call(BaseHTTPRequestHandler):
         query = parsed_path.query
         self.send_response(200)
         self.end_headers()
-        self.wfile.write(cyrptoRSA.start_Key())
+
+        start_key = cyrptoRSA.start_key()
+     
+        self.wfile.write(start_key[int(query)])
        
         return
         
--- httpServer/cyrptoRSA.py
+++ httpServer/cyrptoRSA.py
@@ -5,8 +5,8 @@ def start_Key():
     rsa_key = RSA.generate(bits=2048)
     key = RSA.construct(map(int, (rsa_key.n,rsa_key.e,rsa_key.d)))
 
-    private = key.exportKey()
     public = key.publickey().exportKey()
-    print(public)
-    return public
+    private = key.exportKey()
+
+    return public,private
 
