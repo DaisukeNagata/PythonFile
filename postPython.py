5c9f8281dcd06524883283e17777ebbbaddc1a8f
daisukenagata <dbank0208@gmail.com>
2019-05-26 08:08:42 +0900

    add

--- postPython.py
+++ postPython.py
@@ -11,7 +11,7 @@ def main():
   
     start = time.time()
    
-    r = requests.get('URLSet')
+    r = requests.get('http://localhost:8000')
  
     elapsed_time =  time.time() - start
     text = datetime.now().strftime("%Y/%m/%d %H:%M:%S")+" "+"StatusCode"+str(r.status_code)+" "+"経過時間"+str(elapsed_time)+"\n"
